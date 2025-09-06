7fc8be225edf8fe93447a2cba6c8f66f7a1048b4
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-09-06 14:56:39 +0300

    Update spawner grid

--- src/components/SpawnerGrid/index.tsx
+++ src/components/SpawnerGrid/index.tsx
@@ -1,4 +1,5 @@
 const TOTAL_SPAWNERS: number = 49;
+const GRID_DIMENSION: number = 7;
 
 interface SpawnerGridProps {
     renderItem: (key: number, id: number) => React.ReactNode;
@@ -7,7 +8,12 @@ interface SpawnerGridProps {
 export function ItemsGrid({ renderItem }: SpawnerGridProps) {
     return (
         <div className="grid grid-cols-7 grid-rows-7 gap-1 max-w-fit">
-            {Array.from({length: TOTAL_SPAWNERS}).map((_, id) => renderItem(id, id))}
+            {Array.from({length: TOTAL_SPAWNERS}).map((_, index) => {
+                const col = index % GRID_DIMENSION;
+                const row = Math.floor(index / GRID_DIMENSION);
+                const id = (GRID_DIMENSION - 1 - row) * GRID_DIMENSION + col;
+                return renderItem(index, id);
+            })}
         </div>
     )
 }
