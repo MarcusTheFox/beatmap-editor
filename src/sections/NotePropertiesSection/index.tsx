a97c51d945d91d079908967ea84e1282309fc640
MarcusTheFox <menjmenj@gmail.com>
2025-09-12 17:26:27 +0300

    fix NodePropertiesSection styles

--- src/sections/NotePropertiesSection/index.tsx
+++ src/sections/NotePropertiesSection/index.tsx
@@ -9,7 +9,7 @@ export function NotePropertiesSection() {
             <CardHeader>
                 <CardTitle>Note Properties</CardTitle>
             </CardHeader>
-            <CardBody className="pt-0 grid grid-cols-2 gap-4">
+            <CardBody className="pt-0 grid grid-cols-2 grow-0 gap-4">
                 <Input type="number" readOnly value={"1"} label="Spawner ID" labelPlacement="outside-top"/>
                 <Input type="number" readOnly value={"1"} label="Beat" labelPlacement="outside-top"/>
                 <Input type="number" defaultValue={"1500"} min={0} label="Power" labelPlacement="outside-top"/>
