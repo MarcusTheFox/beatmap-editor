0562551088d550b9780d368fcbc16d472bf75ba1
MarcusTheFox <menjmenj@gmail.com>
2025-10-15 13:59:45 +0300

    add BeforeUnload warning

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -6,8 +6,18 @@ import { NotePropertiesSection } from "@/sections/NotePropertiesSection";
 import { TimelineSection } from "@/sections/TimelineSection";
 import { ExportSection } from "@/sections/ExportSection";
 import { EditorProvider } from "@/contexts/EditorContext";
+import { useEffect } from "react";
 
 export default function EditorPage() {
+    useEffect(() => {
+        const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+            e.preventDefault();
+        };
+
+        window.addEventListener("beforeunload", handleBeforeUnload);
+        return () => window.removeEventListener("beforeunload", handleBeforeUnload);
+    }, []);
+
     return (
         <EditorProvider>
             <EditorLayout>
