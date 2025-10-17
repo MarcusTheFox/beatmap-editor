f8d6fbd86e814b79a8acb46d6c1873d60fc44777
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-10-17 14:18:04 +0300

    Update Navbar and remove upload and export sections from page

--- src/layouts/EditorLayout/index.tsx
+++ src/layouts/EditorLayout/index.tsx
@@ -3,6 +3,9 @@ import { ReactNode } from "react";
 import { ThemeSwitch } from "@/components/theme-switch";
 import { GameLogo } from "@/components/icons";
 import { Link } from "@heroui/link";
+import { Icon20DownloadOutline, Icon20ListBulletOutline, Icon20SquareOutline } from "@vkontakte/icons";
+import { Button } from "@heroui/button";
+import { ExportButton } from "@/components/ExportButton";
 
 interface EditorLayoutProps {
   children: ReactNode;
@@ -11,9 +14,9 @@ interface EditorLayoutProps {
 export function EditorLayout(props: EditorLayoutProps) {
     return (
         <div className="relative flex flex-col h-screen">
-            <Navbar maxWidth="xl">
+            <Navbar maxWidth="2xl">
                 <NavbarContent>
-                    <NavbarBrand>
+                    <NavbarBrand className="max-w-fit mr-10">
                         <Link
                             className="flex justify-start items-center gap-2"
                             color="foreground"
@@ -23,12 +26,39 @@ export function EditorLayout(props: EditorLayoutProps) {
                             <p className="font-bold text-inherit">Beam & Beat</p>
                         </Link>
                     </NavbarBrand>
-                </NavbarContent>
-                <NavbarContent justify="end">
                     <ThemeSwitch />
                 </NavbarContent>
+                <NavbarContent justify="center">
+                    <Button
+                        as={Link}
+                        href="#"
+                        variant="light"
+                        startContent={<Icon20SquareOutline />}
+                    >
+                        Notes
+                    </Button>
+                    <Button
+                        as={Link}
+                        href="#"
+                        variant="light"
+                        startContent={<Icon20ListBulletOutline />}
+                    >
+                        Details
+                    </Button>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <ExportButton
+                        variant="ghost"
+                        color="primary"
+                        startContent={<Icon20DownloadOutline />}
+                    >
+                        Export
+                    </ExportButton>
+                </NavbarContent>
             </Navbar>
-            <main id="main-container" className="flex flex-col gap-4 m-8 h-full">{props.children}</main>
+            <main id="main-container" className="flex flex-col gap-4 m-8 h-full">
+                {props.children}
+            </main>
             <footer />
         </div>
     );
--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,10 +1,8 @@
 import { EditorLayout } from "@/layouts/EditorLayout";
-import { UploadAudioSection } from "@/sections/UploadAudioSection";
 import { TrackInfoSection } from "@/sections/TrackInfoSection";
 import { SpawnerGridSection } from "@/sections/SpawnerGridSection";
 import { NotePropertiesSection } from "@/sections/NotePropertiesSection";
 import { TimelineSection } from "@/sections/TimelineSection";
-import { ExportSection } from "@/sections/ExportSection";
 import { EditorProvider } from "@/contexts/EditorContext";
 import { useEffect } from "react";
 import { EditorNotFound } from "./editorNotFound";
@@ -30,15 +28,9 @@ export default function EditorPage() {
         <EditorProvider>
             <EditorLayout>
                 <div className="grid grid-cols-[1fr_2fr_1fr] justify-stretch">
-                    <div className="flex flex-col gap-4">
-                        <UploadAudioSection />
-                        <TrackInfoSection />
-                    </div>
+                    <TrackInfoSection />
                     <SpawnerGridSection />
-                    <div className="flex flex-col gap-4">
-                        <ExportSection />
-                        <NotePropertiesSection />
-                    </div>
+                    <NotePropertiesSection />
                 </div>
                 <TimelineSection />
             </EditorLayout>
