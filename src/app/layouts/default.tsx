f71117fa9a0531a9a207f9fadb0733a2946562d6
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-11-18 01:48:51 +0300

    fix sticky navbar

--- src/app/layouts/default.tsx
+++ src/app/layouts/default.tsx
@@ -7,7 +7,7 @@ export const DefaultLayout = ({
     children : React.ReactNode
 }) => {
     return (
-        <div className="flex flex-col min-h-screen overflow-x-hidden">
+        <div className="flex flex-col h-screen overflow-x-hidden">
             <Navbar />
             <main className="flex-1">
                 {children}
