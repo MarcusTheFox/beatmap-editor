9a03d804563ff59acafc1bcba76bea0f9aa42a98
Marcus Moore <61185805+MarcusTheFox@users.noreply.github.com>
2025-11-17 22:16:46 +0300

    Update default layout and footer

--- src/widgets/layouts/default.tsx
+++ src/widgets/layouts/default.tsx
@@ -1,5 +1,5 @@
-import { Link } from "@heroui/link";
-import { Navbar } from "../page-navbar";
+import { Footer } from "@/src/widgets/layouts";
+import { Navbar } from "@/src/widgets/page-navbar";
 
 export const DefaultLayout = ({
     children
@@ -7,22 +7,12 @@ export const DefaultLayout = ({
     children : React.ReactNode
 }) => {
     return (
-        <div className="relative flex flex-col h-screen">
+        <div className="relative flex flex-col h-screen overflow-x-hidden">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="grow">
                 {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3">
-                <Link
-                    isExternal
-                    className="flex items-center gap-1 text-current"
-                    href="https://heroui.com?utm_source=next-app-template"
-                    title="heroui.com homepage"
-                >
-                    <span className="text-default-600">Powered by</span>
-                    <p className="text-primary">HeroUI</p>
-                </Link>
-            </footer>
+            <Footer />
         </div>
     );
 }
